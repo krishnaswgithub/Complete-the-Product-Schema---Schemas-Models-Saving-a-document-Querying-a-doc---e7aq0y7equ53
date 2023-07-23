2a5a30568f3640c48a760bd8e14c49a2a2e24b44
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-07-23 21:35:02 +0530

    Update product.js

--- models/product.js
+++ models/product.js
@@ -1,6 +1,28 @@
+// var mongoose= require("mongoose");
 
-var mongoose= require("mongoose");
+// //write your schema Here with name of schema as productSchema
 
-//write your schema Here with name of schema as productSchema
+// module.exports =mongoose.model("products",productSchema);
+var mongoose = require("mongoose");
 
-module.exports =mongoose.model("products",productSchema);
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  Quantity: {
+    type: Number,
+    required: true,
+  },
+  Price: {
+    type: Number,
+    required: true,
+  },
+  instock: {
+    type: String,
+    enum: ["Yes", "No"],
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("products", productSchema);
